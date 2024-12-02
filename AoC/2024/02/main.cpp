38a5ef008551215e87436d24d18c59dd615747ff
adriatp <tarradas.adria@gmail.com>
2024-12-02 15:22:07 +0100

    add part 2 to ch2 in aoc

--- AoC/2024/02/main.cpp
+++ AoC/2024/02/main.cpp
@@ -18,6 +18,55 @@ vector<vector<int>> readFile(const string& filename) {
   return v_v_int;
 }
 
+vector<vector<int>> remove_one(vector<int> v_int) {
+  vector<vector<int>> v_v_int;
+  for (size_t i = 0; i<v_int.size(); i++) {
+    vector<int> tmp = v_int;
+    tmp.erase(tmp.begin() + i);
+    v_v_int.push_back(tmp);
+  }
+  return v_v_int;
+}
+
+bool is_safe(vector<int> level) {
+    int j=0;
+    bool safe=true, asc=(level[0]-level[1] < 0);
+    while (j<level.size()-1 && safe) {
+      int diff = level[j] - level[j+1];
+      if (diff == 0 || (diff > 0 && asc) || (diff < 0 && !asc) || abs(diff) > 3)
+        safe = false;
+      j++;
+    }
+    return safe;
+}
+
+void part_one(vector<vector<int>> levels) {
+  int safes = 0;
+  for (int i=0; i<levels.size(); i++)
+    if (is_safe(levels[i])) safes++;
+  cout << "Safes: " << safes << endl;
+}
+
+void part_two(vector<vector<int>> levels) {
+  int safes = 0;
+  for (int i=0; i<levels.size(); i++) {
+    if (is_safe(levels[i])) safes++;
+    else {
+      vector<vector<int>> sublevels = remove_one(levels[i]);
+      int i=0;
+      bool safe = false;
+      while (i<sublevels.size() && !safe) {
+        if (is_safe(sublevels[i])) {
+          safe = true;
+          safes++;
+        }
+        i++;
+      }
+    }
+  }
+  cout << "Safes: " << safes << endl;
+}
+
 int main() {
   vector<vector<int>> levels = readFile("input.txt");
   // for (int i=0; i<levels.size(); i++) {
@@ -26,18 +75,7 @@ int main() {
   //   }
   //   cout << endl;
   // }
-  int safes = 0;
-  for (int i=0; i<levels.size(); i++) {
-    int j=0;
-    bool safe=true, asc=(levels[i][0]-levels[i][1] < 0);
-    while (j<levels[i].size()-1 && safe) {
-      int diff = levels[i][j] - levels[i][j+1];
-      if (diff == 0 || (diff > 0 && asc) || (diff < 0 && !asc) || abs(diff) > 3)
-        safe = false;
-      j++;
-    }
-    if (safe) safes++;
-  }
-  cout << "Safes: " << safes << endl;
+  part_one(levels);
+  part_two(levels);
   return 0;
 }
